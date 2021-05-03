9518fb173be08b72ac42fffd65dad793e665b1fc
Mircea Voivod <mircea@voivod.family>
2021-05-03 17:28:41 +0300

    added placeholders

---
+++ resources/lang/en/xls.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'upload' => 'Upload XLS',
+    'delete' => 'Delete',
+    'search' => 'Search XLS',
+    'count' => 'Found in :count files',
+    'triplestar' => 'Use *** between words. Example:  lotzi***iuan will search rows with both lotzi and iuan in some columns.'
+];
